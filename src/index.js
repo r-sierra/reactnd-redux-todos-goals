3b06ed70d59abab7176187e7229f786dc15f9a4d
r-sierra <roberto@ideadigital.com.ar>
2021-07-19 16:26:54 -0300

    Store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import './index.css'
-import App from './components/App'
+import ConnectedApp from './components/App'
+import reducer from './reducers'
+import middleware from './middleware'
+
+const store = createStore(reducer, middleware)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReactRedux.Provider store={store} >
+      <ConnectedApp />
+    </ReactRedux.Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 )
